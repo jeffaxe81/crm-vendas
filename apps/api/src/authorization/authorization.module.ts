dc9c0f388e489305d834c9dabe347cecef30b4d9
jeffaxe81 <jeffersonmachado81@gmail.com>
2026-09-01 13:30:28 -0700

    feat(authz): register protected session controller

--- apps/api/src/authorization/authorization.module.ts
+++ apps/api/src/authorization/authorization.module.ts
@@ -4,9 +4,11 @@ import { AuthModule } from "../auth/auth.module";
 import { DatabaseModule } from "../database/database.module";
 import { AuthenticationGuard } from "./authentication.guard";
 import { PermissionsGuard } from "./permissions.guard";
+import { SessionController } from "./session.controller";
 
 @Module({
   imports: [AuthModule, DatabaseModule],
+  controllers: [SessionController],
   providers: [AuthenticationGuard, PermissionsGuard],
   exports: [AuthenticationGuard, PermissionsGuard],
 })
